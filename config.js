9dd63547f6309783a66cddbec454d15e0fb0869f
hxbchau24nt <hxbchau.24nt127@huemed-univ.edu.vn>
2025-07-22 22:29:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,20 @@
-const CONFIG = {
-    titleWeb: "Our special day",
-    introTitle: 'Xin chào em, Lê Thị Thu Hiền',
-    introDesc: `Hôm qua đi chợ mua chanh,
+window.CONFIG = {
+  titleWeb: "Our special day",
+  introTitle: "Xin chào em, Lê Thị Thu Hiền",
+  introDesc: `Hôm qua đi chợ mua chanh,
 Về nhà mới nhớ… thiếu em bên đời.
 Chanh chua nhưng vẫn còn tươi,
 Còn em vừa ngọt, vừa cười là anh tan`,
-    btnIntro: '^^HiHi^',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Người đâu gặp gỡ làm chi trăm năm biết có duyên dì hay không',
-    btnReply: 'Hữu duyênnnn',
-    reply: 'Thôi thì chẳng có gì ',
-    mess: ' Chắc là anh yêu nhiều tại em đó🥰',
-    messDesc: 'Tối thứ sáu tuần này 18h30 25/6/2025, anh qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/baochau10a2' 
-};
-
-export default CONFIG;
+  btnIntro: "^^HiHi^",
+  title: "Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰",
+  desc: "Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Thích lắm <33",
+  btnNo: "Không nha :3",
+  question: "Người đâu gặp gỡ làm chi trăm năm biết có duyên gì hay không",
+  btnReply: "Hữu duyênnnn",
+  reply: "Thôi thì chẳng có gì ",
+  mess: "Chắc là anh yêu nhiều tại em đó🥰",
+  messDesc: "Tối thứ sáu tuần này 18h30 25/6/2025, anh qua đón đi chơi nha.",
+  btnAccept: "Okiiiii lun <3",
+  messLink:
 
