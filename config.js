51ace967ea1a5518cd7bfc48c3d20945fc19e97d
hxbchau24nt <hxbchau.24nt127@huemed-univ.edu.vn>
2025-07-22 21:33:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Our special day",
+    introTitle: 'Xin chào em, Lê Thị Thu Hiền',
+    introDesc: `Hôm qua đi chợ mua chanh,
+Về nhà mới nhớ… thiếu em bên đời.
+Chanh chua nhưng vẫn còn tươi,
+Còn em vừa ngọt, vừa cười là anh tan`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    question: 'Người đâu gặp gỡ làm chi trăm năm biết có duyên gì hay không',
+    btnReply: 'Hữu duyênnnn',
+    reply: 'Thôi thì chẳng có gì ',
+    mess: ' Chắc là anh yêu nhiều tại em đó🥰',
+    messDesc: 'Tối thứ sáu tuần này 18h30 25/6/2025, anh qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    messLink: 'https://www.facebook.com/baochau10a2' 
